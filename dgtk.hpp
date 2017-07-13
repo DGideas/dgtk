0107bd4636f02adce67284f7885e6ff4a58cddc3
DGideas <dgideas@outlook.com>
2017-07-13 14:41:53 +0800

    cppdas::readcsv()

--- dgtk.hpp
+++ dgtk.hpp
@@ -33,26 +33,60 @@ namespace dgtk
 			}
 	};
 	
+	// Like Python pandas, cppdas is a class about C++ data analysis.
+	// If you use:
+	// using pd = dgtk::cppdas;
+	// It looks like python pandas :)
 	class cppdas
 	{
 		public:
+			static const std::vector<std::string> defaultSplitCharacter;
 			static cppdas_dataset read_csv(const std::string&,
-				const std::vector<std::string>&);
-			static cppdas_dataset read_csv(std::ifstream&,
-				const std::vector<std::string>&);
+				const std::vector<std::string>&, const bool&);
+			static cppdas_dataset read_csv(const std::string&,
+				const bool&);
+			static cppdas_dataset read_csv(std::fstream&,
+				const std::vector<std::string>&, const bool&);
+			static cppdas_dataset read_csv(std::fstream&,
+				const bool&);
+			static cppdas_dataset read_csv_base(std::fstream&,
+				const std::vector<std::string>&, const bool&);
 	};
 	
+	const std::vector<std::string> cppdas::defaultSplitCharacter = {","};
+	
 	cppdas_dataset cppdas::read_csv(const std::string& _string,
-		const std::vector<std::string>& _split)
+		const std::vector<std::string>& _split = {","},
+		const bool& _haveHeader = true)
 	{
-		cppdas_dataset a;
-		return a;
+		std::fstream fileHandle(_string, std::ios::in);
+		return read_csv_base(fileHandle, _split, _haveHeader);
 	}
 	
-	cppdas_dataset cppdas::read_csv(std::ifstream&,
-		const std::vector<std::string>& _split)
+	cppdas_dataset cppdas::read_csv(const std::string& _string,
+		const bool& _haveHeader)
 	{
-		cppdas_dataset a;
-		return a;
+		std::fstream fileHandle(_string, std::ios::in);
+		return read_csv_base(fileHandle, defaultSplitCharacter, _haveHeader);
+	}
+	
+	cppdas_dataset cppdas::read_csv(std::fstream& _fsHandle,
+		const std::vector<std::string>& _split = {","},
+		const bool& _haveHeader = true)
+	{
+		return read_csv_base(_fsHandle, _split, _haveHeader);
+	}
+	
+	cppdas_dataset cppdas::read_csv(std::fstream& _fsHandle,
+		const bool& _haveHeader)
+	{
+		return read_csv_base(_fsHandle, defaultSplitCharacter, _haveHeader);
+	}
+	
+	cppdas_dataset cppdas::read_csv_base(std::fstream& _fsHandle,
+		const std::vector<std::string>& _split,
+		const bool& _haveHeader)
+	{
+		
 	}
 }
