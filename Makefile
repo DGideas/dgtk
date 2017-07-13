5a7b00293d1432a74cdcd8220bef075a664679e0
DGideas <dgideas@outlook.com>
2017-07-13 14:24:49 +0800

    Minor changes and improvements

--- Makefile
+++ Makefile
@@ -3,6 +3,16 @@ dgtkCoreFiles = dgtk.cpp dgtk.hpp
 make: $(dgtkCoreFiles)
 	g++ dgtk.cpp -o dgtk --std=c++14
 
-test: $(dgtkCoreFiles)
+compile: $(dgtkCoreFiles)
 	g++ dgtk.cpp -o dgtk --std=c++14
 	rm dgtk
+
+test: $(dgtkCoreFiles)
+	g++ dgtk.cpp -o dgtk --std=c++14
+	./dgtk
+	rm dgtk
+
+gitminorchanges:
+	git add -A .
+	git commit -m "Minor changes and improvements"
+	git push
--- dgtk.hpp
+++ dgtk.hpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <utility>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <stack>
@@ -14,6 +15,44 @@
 #include <cstdlib>
 
 namespace dgtk
+// All components in DGTK are defined in dgtk namespace.
+// In order to avoid look-up ambiguous, using std:: prefix
+// when you use standard library components.
 {
+	// cppdas_dataset is a class that storage linear dataset.
+	class cppdas_dataset
+	{
+		public:
+			cppdas_dataset()
+			{
+				std::cout<<"Construct"<<std::endl;
+			}
+			~cppdas_dataset()
+			{
+				std::cout<<"distruct"<<std::endl;
+			}
+	};
 	
+	class cppdas
+	{
+		public:
+			static cppdas_dataset read_csv(const std::string&,
+				const std::vector<std::string>&);
+			static cppdas_dataset read_csv(std::ifstream&,
+				const std::vector<std::string>&);
+	};
+	
+	cppdas_dataset cppdas::read_csv(const std::string& _string,
+		const std::vector<std::string>& _split)
+	{
+		cppdas_dataset a;
+		return a;
+	}
+	
+	cppdas_dataset cppdas::read_csv(std::ifstream&,
+		const std::vector<std::string>& _split)
+	{
+		cppdas_dataset a;
+		return a;
+	}
 }
